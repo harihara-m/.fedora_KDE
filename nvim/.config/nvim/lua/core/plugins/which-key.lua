dbefd252193333eca097236c83d5353045c9f67d
Harihara Maharna <hariharamaharna1644@gmail.com>
2024-09-25 18:45:06 -0400

    Snippets updates

--- nvim/.config/nvim/lua/core/plugins/which-key.lua
+++ nvim/.config/nvim/lua/core/plugins/which-key.lua
@@ -15,4 +15,24 @@ return {
             desc = "Buffer Local Keymaps (which-key)",
         },
     },
+    config = function()
+        local wk = require("which-key")
+        wk.add({
+            { "<leader><tab>", group = "tab", icon = "󰓩" },
+            { "<leader>U", group = "undo", icon = "󰕍" },
+            { "<leader>b", group = "buffer", icon = "" },
+            { "<leader>c", group = "code", icon = "" },
+            { "<leader>e", group = "yazi", icon = "󰇥" },
+            { "<leader>f", group = "find/file", icon = "" },
+            { "<leader>g", group = "git", icon = "" },
+            { "<leader>l", group = "lazy", icon = "󰒲" },
+            { "<leader>o", group = "options/toggle", icon = "" },
+            { "<leader>q", group = "sessions", icon = "" },
+            { "<leader>r", group = "reload", icon = "󰑓" },
+            { "<leader>s", group = "split", icon = "" },
+            { "<leader>t", group = "terminal", icon = "" },
+            { "<leader>u", group = "ui" },
+            { "<leader>?", group = "which-key", icon = "" },
+        })
+    end,
 }
